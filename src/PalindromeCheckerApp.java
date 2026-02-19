b16e948012cbcc5f51e2739cbe6126f7e064599c
Riddh1ma <singhriddhima19@gmail.com>
2026-02-19 09:07:20 +0530

    Initial commit PalindromeCheckerApp

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerApp {
+        static final String APP_NAME = "Palindrome Checker App";
+        static final String VERSION = "Version 1.0";
+
+        // Main method - Entry point of the application
+        public static void main(String[] args) {
+
+            // Welcome Message
+            System.out.println("        " + APP_NAME);
+            System.out.println("        " + VERSION);
+            System.out.println("Welcome to the Palindrome Checker Application!");
+            System.out.println("This application checks whether a given string is a palindrome.");
+
+            // End of UC1 - Program ready for next use case
+            System.out.println("Application initialized successfully.");
+        }
+}
